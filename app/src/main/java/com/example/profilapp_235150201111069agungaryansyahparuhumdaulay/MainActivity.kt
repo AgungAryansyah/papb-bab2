b2538f0ab06edc4653eb45b9f1fd6d3e7de2c0b0
agungaryansyah <agungaryansyah2006@gmail.com>
2025-09-22 23:54:20 +0700

    feat: tugas praktikum

--- app/src/main/java/com/example/profilapp_235150201111069agungaryansyahparuhumdaulay/MainActivity.kt
+++ app/src/main/java/com/example/profilapp_235150201111069agungaryansyahparuhumdaulay/MainActivity.kt
@@ -3,45 +3,93 @@ package com.example.profilapp_235150201111069agungaryansyahparuhumdaulay
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.profilapp_235150201111069agungaryansyahparuhumdaulay.ui.theme.ProfilApp_235150201111069AgungAryansyahParuhumDaulayTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ProfilApp_235150201111069AgungAryansyahParuhumDaulayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Profile()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Preview(){
     ProfilApp_235150201111069AgungAryansyahParuhumDaulayTheme {
-        Greeting("Android")
+        Profile()
     }
+}
+
+@Composable
+fun FollowButton() {
+    var isFollowed by remember { mutableStateOf(false) }
+    Button(onClick = { isFollowed = !isFollowed }) {
+        Text(if (isFollowed) "Unfollow" else "Follow")
+    }
+}
+
+@Composable
+fun Profile(){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Foto Profil",
+            modifier = Modifier.size(120.dp).clip(CircleShape).padding(6.dp).background(Color.Blue)
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text("Nama: Agung", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text("NIM: 235150201111069", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text("Mahasiswa Teknik Informatika")
+        Spacer(modifier = Modifier.height(8.dp))
+        FollowButton()
+    }
+}
+
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
